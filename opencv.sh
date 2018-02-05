1308a46e9b79240917914e9dc4c5c0374bd69ee9
Jitendra Mishra <jitendra29mishra@gmail.com>
2018-02-06 01:08:57 +0530

    update opencv.sh

--- opencv.sh
+++ opencv.sh
@@ -1,90 +1,75 @@
+!/bin/bash
 
-version="$(wget -q -O - http://sourceforge.net/projects/opencvlibrary/files/opencv-unix | egrep -m1 -o '\"[0-9](\.[0-9]+)+' | cut -c2-)"
-echo "Installing OpenCV" $version
+## install git
+sudo apt install git
 
-mkdir OpenCV
-cd OpenCV
-echo "Removing any pre-installed ffmpeg and x264"
-echo "sudo apt-get remove x264 libx264-dev"
-echo "***********************************"
+## download opencv from git
+git clone https://github.com/opencv/opencv.git
 
-echo "Installing Dependenices"
-sudo apt-get install libopencv-dev
+## change directory
+cd opencv
 
-echo "************Build Tools***********************"
+## download opencv_contrib from git
+git clone https://github.com/opencv/opencv_contrib.git
 
-echo "<----------------------------------------------------Build Tools------------------------------------------------->"
-sudo apt-get install build-essential checkinstall cmake pkg-config
-echo "*_*_*_*_*_*_*_*_*_*_*_*_*_*"
+## install supporting files
+sudo chmod +x requiredlibrary.sh
+./requiredlibrary.sh
 
-
-echo "<------------------------------------------------------Image I/O----------------------------------------------------->"
-sudo apt-get install libtiff5-dev libjpeg-dev libjasper-dev libpng-dev zliblg-dev libwebp-dev libopenexr-dev libgdal-dev
-echo "***********************************"
-
-echo "<--------------------------------------------------------Video I/O--------------------------------------------------->"
-sudo apt-get install libavcodec-dev libavformat-dev libmp3lame-dev
-sudo apt-get install libswscale-dev libdc1394-22-dev libxine-dev
-sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
-sudo apt-get install libv4l-dev v4l-utils libfaac-dev libopencore-amrnb-dev
-sudo apt-get instal libopencore-amrwb-dev libtheora-dev libvorbis-dev
-sudo apt-get insta libxvidcore-dev libx264-dev x264 yasm
-echo "***********************************"
-
-echo "Parallelism and linear algebra libraries"
-sudo apt-get install libtbb-dev libeigen3-dev
-echo "***********************************"
-
-echo "<------------------------------------------------for GUI------------------------------------------------->"
-sudo apt-get install libqt4-dev libgtk2.0-dev qt5-default
-echo " sudo apt-get install libvtk6-dev"
-
-echo "*************************************************************************************************************"
-
-echo "<--------------For JAVA-------------------->"
-echo sudo apt-get install ant default-jdk
-
-echo "<-------********------For Python------********-------->"
-echo sudo apt-get install python-dev python-tk python-numpy python3-dev python3-tk python3-numpy python-matplotlib
-sudo apt-get install python-opencv
-
-echo "%_%_%_%_%_%_%_%_%_%_%_%_%_%_%_%_%_%_%_%_%_%"
-echo "Downloading OpenCV" $version
-wget -O OpenCV-$version.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/opencv-"$version".zip/download
-echo "Installing OpenCV" $version
-unzip OpenCV-$version.zip
-cd opencv-$version
+## make build directory
 mkdir build
 cd build
 
-echo "*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_"
+#echo "*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_"
+
+make clean
+#./javaConf.sh
+#export JAVA_HOME=/usr/local/lib/jvm/jdk1.8.0_144
+#export ANT_HOME=/media/jitendra/Jitu/SoftwarePrograming/Java/Android/apache-ant-1.9.9/bin
 
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
 	-D CMAKE_INSTALL_PREFIX=/usr/local \
-	-D WITH_TBB=ON \
-	-D BUILD_NEW_PYTHON_SUPPORT=ON \
-	-D WITH_V4L=ON \
-  	-D BUILD_opencv_java=ON \
-	-D INSTALL_C_EXAMPLES=ON \
+	-D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
 	-D INSTALL_PYTHON_EXAMPLES=ON \
-	-D BUILD_DOCS=ON \
+	-D INSTALL_C_EXAMPLES=ON \
+	-D BUILD_NEW_PYTHON_SUPPORT=ON \
 	-D BUILD_EXAMPLES=ON \
-	-D WITH_QT=ON \
 	-D WITH_OPENGL=ON \
-	-D WITH_EIGEN=ON ..
+	-D WITH_OPENMP=ON \
+	-D WITH_OPENCL=ON \
+	-D BUILD_DOCS=ON \
+	-D WITH_EIGEN=ON \
+	-D BUILD_TEST=ON \
+	-D WITH_GTK=ON \
+	-D WITH_TBB=ON \
+	-D WITH_V4L=ON \
+	-D WITH_QT=ON \
+	-D BUILD_opencv_java=ON \
+	-D BUILd_SHARED_LIBS=OFF ..
 
-make -j4
+    #-D BUILD_opencv_java=ON \
+	#-D PYTHON_DEFAULT_EXECUTABLE=~/anaconda3/bin/python \
+	#-D WITH_VTK=ON \
+	#-D FORCE_VTK=TRUE \
+# for configure python
+	#-D PYTHON_DEFAULT_EXECUTABLE=$PYTHON/bin/python3 \
+	#-D PYTHON_EXECUTABLE=$PYTHON3/bin/python3 \
+	#-D PYTHON_INCLUDE_DIR=$PYTHON3/include/python3.5m \
+	#-D PYTHON_INCLUDE_DIR2=/usr/include/x86_64-linux-gnu/python3.5m \
+	#-D PYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.5m.so.1 \
+	#-D PYTHON_NUMPY_INCLUDE_DIRS=$PYTHON3/lib/python3.4/site-packages/numpy/core/include	\
 
-echo "***********************************"
-
+#---------------------------------------------------------------------------------------------#
+make check_pylint
+make -j8
 sudo make install
-
-echo "***********************************"
-
 sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
-
-echo "***********************************"
-
 sudo ldconfig
+#---------------------------------------------------------------------------------------------#
 
-echo "OpenCV" $version "ready to be used"
+echo "<---------------- OpenCV ready to be used ---------------->"
+cd ../..
+sudo chmod +x compileOpencv.sh
+cp compileOpencv.sh ~/.compileOpencv.sh
+echo "alias opencv='~/./.compileOpencv.sh'" >> ~/.bash_aliases
+source ~/.bash_aliases
